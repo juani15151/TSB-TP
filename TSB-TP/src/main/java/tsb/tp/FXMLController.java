706bbdff669c72bdd1ceff3c1c0475f4203b92b7
Codes <Codes@DESKTOP-B012JPO>
2017-11-11 20:15:52 -0300

    Cambios: Cambiando weas.

--- TSB-TP/src/main/java/tsb/tp/FXMLController.java
+++ TSB-TP/src/main/java/tsb/tp/FXMLController.java
@@ -50,13 +50,11 @@ public class FXMLController implements Initializable {
     private void CargarArchivo(ActionEvent event) {
         // Creamos los filtros para el FileChooser
         ExtensionFilter txtFilter = new ExtensionFilter("Text files (.txt)","*.txt");
-        ExtensionFilter pdfFilter = new ExtensionFilter("PDF files (.pdf)","*.pdf");
         ExtensionFilter allFilter = new ExtensionFilter("All files (*)","*");
         // Creamos el FileChooser
         FileChooser fileChooser = new FileChooser();
         // Agregamos los filtros al FileChooser
         fileChooser.getExtensionFilters().add(txtFilter);
-        fileChooser.getExtensionFilters().add(pdfFilter);
         fileChooser.getExtensionFilters().add(allFilter);
         // Ponemos titulo al FileChooser
         fileChooser.setTitle("Buscar archivo");
@@ -79,12 +77,37 @@ public class FXMLController implements Initializable {
     }
     
     private void cargarHash(){
+        int[] counter = new int[lstPalabras.getItems().size()];
+        
+        File file = new File(tfArchivo.getText());
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] str = line.split(" ");
+                for (int i = 0; i < str.length; i++) {
+                    for(int j = 0; j < counter.length; j++){
+                        if (str[i].equals(lstPalabras.getItems().get(j))) {
+                            counter[j]++;
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("nada que hacer");
+        }
+        
+        for(int i = 0; i < lstPalabras.getItems().size(); i++){
+            table.put(lstPalabras.getItems().get(i), counter[i]);
+        }
+        
+        /*
         for(int i = 0; i < lstPalabras.getItems().size(); i++){
             String palabra = lstPalabras.getItems().get(i);
             System.out.println("Palabra: " + palabra + " Repeticiones: " + BuscarPalabra(palabra));
             table.put(palabra, BuscarPalabra(palabra));
         }
-        System.out.println(table);
+        System.out.println(table);*/
     }
     
     private void cargarLista(File file){
@@ -94,7 +117,8 @@ public class FXMLController implements Initializable {
             while ((line = br.readLine()) != null) {
                 String[] str = line.split(" ");
                 for(int i = 0; i < str.length; i++){
-                    if(!str[i].equals(" ")&&!str[i].isEmpty() && !lstPalabras.getItems().contains(str[i])){
+                    str[i] = checkPalabra(str[i]);
+                    if( !str[i].contains("www") &&!str[i].equals(" ")&& !str[i].isEmpty() && !lstPalabras.getItems().contains(str[i])){
                         lstPalabras.getItems().add(str[i]);
                     }
                 }
@@ -158,5 +182,37 @@ public class FXMLController implements Initializable {
 
             alert.showAndWait();
         }
+    }
+    public String checkPalabra(String palabra){
+        palabra = palabra.replace(".", "");
+        palabra = palabra.replace(",", "");
+        palabra = palabra.replace("-", "");
+        palabra = palabra.replace("(", "");
+        palabra = palabra.replace(")", "");
+        palabra = palabra.replace(":", "");
+        palabra = palabra.replace(";", "");
+        palabra = palabra.replace("¿", "");
+        palabra = palabra.replace("?", "");
+        palabra = palabra.replace("_", "");
+        palabra = palabra.replace("*", "");
+        palabra = palabra.replace("¡", "");
+        palabra = palabra.replace("!", "");
+        palabra = palabra.replace("<", "");
+        palabra = palabra.replace(">", "");
+        palabra = palabra.replace("[", "");
+        palabra = palabra.replace("]", "");
+        palabra = palabra.replace("#", "");
+        palabra = palabra.replace("@", "");
+        palabra = palabra.replace("0", "");
+        palabra = palabra.replace("1", "");
+        palabra = palabra.replace("2", "");
+        palabra = palabra.replace("3", "");
+        palabra = palabra.replace("4", "");
+        palabra = palabra.replace("5", "");
+        palabra = palabra.replace("6", "");
+        palabra = palabra.replace("7", "");
+        palabra = palabra.replace("8", "");
+        palabra = palabra.replace("9", "");
+        return palabra;
     }
 }
