758a521a500a698d69bcc8e698d1934548022f52
Codes <Codes@DESKTOP-B012JPO>
2017-11-11 15:42:07 -0300

    Cambios: Agregada busqueda.

--- TSB-TP/src/main/java/tsb/tp/FXMLController.java
+++ TSB-TP/src/main/java/tsb/tp/FXMLController.java
@@ -43,20 +43,29 @@ public class FXMLController implements Initializable {
 
     @FXML
     private void CargarArchivo(ActionEvent event) {
+        // Creamos los filtros para el FileChooser
         ExtensionFilter txtFilter = new ExtensionFilter("Text files (.txt)","*.txt");
         ExtensionFilter pdfFilter = new ExtensionFilter("PDF files (.pdf)","*.pdf");
         ExtensionFilter allFilter = new ExtensionFilter("All files (*)","*");
+        // Creamos el FileChooser
         FileChooser fileChooser = new FileChooser();
+        // Agregamos los filtros al FileChooser
         fileChooser.getExtensionFilters().add(txtFilter);
         fileChooser.getExtensionFilters().add(pdfFilter);
         fileChooser.getExtensionFilters().add(allFilter);
+        // Ponemos titulo al FileChooser
         fileChooser.setTitle("Buscar archivo");
+        
+        // Obtenemos el archivo seleccionado del FileChooser
         File archivo = fileChooser.showOpenDialog(null);
+        // Le seteamos el path al TextField de cargar archivo.
         tfArchivo.textProperty().set(archivo.getPath());
+        // Cargamos la lista
         cargarLista(archivo);
     }
     
     private void cargarLista(File file){
+        lstPalabras.getItems().clear();
         try{
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line = null;
@@ -77,7 +86,7 @@ public class FXMLController implements Initializable {
     @FXML
     private void eventOnMouseclicked(MouseEvent event) {
         try{
-            if (!lstPalabras.getSelectionModel().getSelectedItem().toString().isEmpty() && !lstPalabras.getItems().isEmpty()) {
+            if (!lstPalabras.getSelectionModel().getSelectedItem().isEmpty() && !lstPalabras.getItems().isEmpty()) {
                 tfBusqueda.setText(lstPalabras.getSelectionModel().getSelectedItem().toString());
             }
         } catch(Exception e){}
@@ -85,6 +94,23 @@ public class FXMLController implements Initializable {
 
     @FXML
     private void BuscarPalabra(ActionEvent event) {
+        File file = new File(tfArchivo.getText());
+        int counter = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] str = line.split(" ");
+                for (int i = 0; i < str.length; i++) {
+                    if (str[i].equals(tfBusqueda.getText().trim())) {
+                        counter++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("nada que hacer");
+        }
         
+        tfRepeticiones.setText(""+counter);
     }
 }
