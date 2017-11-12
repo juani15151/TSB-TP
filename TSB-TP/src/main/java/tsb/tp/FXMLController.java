3f3b703172cbf3a5f345b7e16ee012982323c504
juani15151 <wfalconwf@gmail.com>
2017-11-11 21:42:26 -0300

    GitFix.

--- TSB-TP/src/main/java/tsb/tp/FXMLController.java
+++ TSB-TP/src/main/java/tsb/tp/FXMLController.java
@@ -42,7 +42,7 @@ public class FXMLController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // TODO      
         
     }    
 
